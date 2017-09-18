269e84702ef7b67971c1dad08b979c0881b2d4a2
Naoto <codegeekboy@gmail.com>
2017-09-18 09:40:30 +0000

    TODOリスト機能を持ったHubot

---
+++ scripts/todo.js
@@ -0,0 +1,43 @@
+// Description:
+//   TODO を管理することができるボットです
+// Commands:
+//   ボット名 todo     - TODO を作成
+//   ボット名 done     - TODO を完了にする
+//   ボット名 del      - TODO を消す
+//   ボット名 list     - TODO の一覧表示
+//   ボット名 donelist - 完了した TODO の一覧表示
+'use strict';
+const todo = require('todo');
+module.exports = (robot) => {
+    robot.respond(/todo (.+)/i, (msg) => {
+        const task = msg.match[1].trim();
+        todo.todo(task);
+        msg.send('ツイカシマシタ: ' + task);
+    });
+    robot.respond(/done (.+)/i, (msg) => {
+        const task = msg.match[1].trim();
+        todo.done(task);
+        msg.send('カンリョウニシマシタ: ' + task);
+    });
+    robot.respond(/del (.+)/i, (msg) => {
+        const task = msg.match[1].trim();
+        todo.del(task);
+        msg.send('サクジョシマシタ: ' + task);
+    });
+    robot.respond(/list/i, (msg) => {
+		const list = todo.list();
+		if (list.length > 0) {
+			msg.send(todo.list().join('\n'));
+		} else {
+			msg.send('(TODOはありません)');
+		}
+	});
+	robot.respond(/donelist/i, (msg) => {
+		const donelist = todo.donelist();
+		if (donelist.length > 0) {
+			msg.send(todo.donelist().join('\n'));
+		} else {
+			msg.send('(完了したTODOはありません)');
+		}
+	});
+};
